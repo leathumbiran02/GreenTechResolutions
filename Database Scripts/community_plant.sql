7b2b19de8007d7d9609ddee55588b647cfc078a3
Lea Thumbiran <111317155+leathumbiran02@users.noreply.github.com>
2023-07-20 16:33:11 +0200

    Added more fruits and vegetables.

--- Database Scripts/community_plant.sql
+++ Database Scripts/community_plant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2023 at 03:47 PM
+-- Generation Time: Jul 20, 2023 at 04:32 PM
 -- Server version: 8.0.28
 -- PHP Version: 8.2.0
 
@@ -44,7 +44,11 @@ INSERT INTO `community_plant` (`plant_id`, `user_email`, `plant_name`, `descript
 (1, 'leathumbiran@gmail.com', 'Tomato', 'Tomato is a versatile and delicious fruit commonly used in various culinary dishes.', 'https://th.bing.com/th/id/OIP.NNiG9NKcVyJ8fPh66V3WSgHaFW?pid=ImgDet&rs=1', '10.00'),
 (2, 'leathumbiran@gmail.com', 'Cauliflower', 'Cauliflower is a nutritious vegetable with a mild, versatile flavor.', 'https://th.bing.com/th/id/R.a9bb5f4e2784a4e038cbae92634cef10?rik=WkEZnPmHt7FiNQ&riu=http%3a%2f%2fnutrawiki.org%2fwp-content%2fuploads%2f2015%2f09%2fCauliflower.jpg&ehk=McP9NlaloU%2bdei9SFVtIx%2fNOFu1Xsc1nnFpasDVm4xI%3d&risl=&pid=ImgRaw&r=0', '20.00'),
 (3, 'leathumbiran@gmail.com', 'Spinach', 'Spinach is a nutritious leafy green vegetable packed with vitamins and minerals.', 'https://cdn.mos.cms.futurecdn.net/atyrpYQoxdoTzmEgu8HMWE-1200-80.jpg', '30.00'),
-(4, 'leathumbiran@gmail.com', 'Carrot', 'The carrot is a nutritious orange root vegetable.', 'https://th.bing.com/th/id/R.2f44d15737bd7c1f6bd5d843ec289879?rik=MGTrOg5HZ5jVCQ&pid=ImgRaw&r=0', '22.00');
+(4, 'leathumbiran@gmail.com', 'Carrot', 'The carrot is a nutritious orange root vegetable.', 'https://th.bing.com/th/id/R.2f44d15737bd7c1f6bd5d843ec289879?rik=MGTrOg5HZ5jVCQ&pid=ImgRaw&r=0', '22.00'),
+(5, 'leathumbiran@gmail.com', 'Broccoli', 'Broccoli is a nutritious green vegetable.', 'https://th.bing.com/th/id/R.60331a2e67ae8eadeeba0cd68363d114?rik=ZwcO9IoH6suCyw&riu=http%3a%2f%2f2.bp.blogspot.com%2f-jWeRRnyX-VE%2fWQX674gHsLI%2fAAAAAAAABkU%2ftAoB82mDfY0oRe9RU0t4KMUrT9wL8s74ACK4B%2fs1600%2fbroccoli.jpg&ehk=g7eLMymMcibpJDqh%2fxj1tJWeld3lGdv4LAg76SPTrkM%3d&risl=&pid=ImgRaw&r=0', '15.00'),
+(6, 'leathumbiran@gmail.com', 'Bell pepper', 'Bell pepper is a sweet and colorful vegetable.', 'https://th.bing.com/th/id/OIP.wZfzxUAZIGewi3SEIzI43wHaJ4?pid=ImgDet&rs=1', '8.00'),
+(7, 'leathumbiran@gmail.com', ' Strawberries', 'Strawberries are sweet, juicy, and vibrant red fruits loved by many.', 'https://th.bing.com/th/id/OIP.fR3uGTXu0iwcQ9jHnItVzQHaLH?pid=ImgDet&rs=1', '25.00'),
+(8, 'leathumbiran@gmail.com', 'Blueberries', 'Blueberries are small, delicious, and nutritious fruits.', 'https://th.bing.com/th/id/OIP.9wCxblC6uhozuXdGYOFV2AHaFj?pid=ImgDet&rs=1', '35.00');
 
 --
 -- Indexes for dumped tables
@@ -65,7 +69,7 @@ ALTER TABLE `community_plant`
 -- AUTO_INCREMENT for table `community_plant`
 --
 ALTER TABLE `community_plant`
-  MODIFY `plant_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `plant_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
