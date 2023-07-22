15295c4780a4b7cb427a00ac65f774dc978bab0d
Lea Thumbiran <111317155+leathumbiran02@users.noreply.github.com>
2023-07-22 04:34:16 +0200

    Script to recreate the fish_feeders_timer table.

---
+++ Database Scripts/fish_feeder_timers.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 22, 2023 at 04:33 AM
+-- Server version: 8.0.28
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `greentech_resolutions`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fish_feeder_timers`
+--
+
+CREATE TABLE `fish_feeder_timers` (
+  `id` int NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `timer_value` bigint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `fish_feeder_timers`
+--
+
+INSERT INTO `fish_feeder_timers` (`id`, `user_email`, `timer_value`) VALUES
+(8, 'leathumbiran@gmail.com', 1690012800000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `fish_feeder_timers`
+--
+ALTER TABLE `fish_feeder_timers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email_UNIQUE` (`user_email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `fish_feeder_timers`
+--
+ALTER TABLE `fish_feeder_timers`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
