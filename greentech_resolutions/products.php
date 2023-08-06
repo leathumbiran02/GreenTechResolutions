26ac49055fced76d49ddf7e54d69a21873e1bed7
Tonisha <nishagovender0205@gmail.com>
2023-08-06 16:42:15 +0200

    Added styling and content to product .php

--- greentech_resolutions/products.php
+++ greentech_resolutions/products.php
@@ -12,82 +12,128 @@
         <link rel="stylesheet" href="style.css">
 
         <style>
-            .hero{ /* Centering the text: */
+            body {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                position: relative;
+                background-color: white;
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+                background-image: url(https://img.freepik.com/free-vector/background-gradient-green-tones_23-2148360340.jpg?w=740&t=st=1691328607~exp=1691329207~hmac=ff61b043e7e12160cc6dc24d7e96b8f5a4c24750925d0c9d8cad38aca6eff884);
+                background-position:center;
+                background-size:cover;
+                background-attachment: fixed;
+            }
+
+            .container {
+                width: 80%;
+                min-height: 350px;
+                border-radius: 20px; /* Set only the top-left and top-right corners to create a curved top border */
+                position: relative;
+                /* Removed transition and transform-style */
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                margin-left: 27px;
+                background-size: cover;
+            }
+            .side {
+                /* Removed position and backface-visibility */
                 text-align: center;
-            }
-
-            /* Ensure that the table and its cells take the full width of the container */
-            table {
-                width: 100%;
-            }
-            
-            td {
-                width: 33.33%; /* Divide the table width into equal parts for each cell */
-            }
-            
-            /* Set the card class styles */
-            .card {
-                
-                padding: 10px; /* Reduce the padding to make the cards smaller */
-                position: relative;
-                width: 333px; /* Adjust the size as needed */
-                height: 333px; /* Adjust the size as needed */
-                perspective: 1000px; /* Create the 3D effect */
-            }
-
-            .flip-card-inner {
-                position: relative;
-                width: 333px; /* Adjust the size as needed */
-                height: 333px; /* Adjust the size as needed */
-                transform-style: preserve-3d; /* Enable 3D transformations for inner elements */
-                transition: transform 0.5s; /* Add a smooth transition */
-            }
-            
-            /* Set the image styles inside the cards */
-            .card img {
-                width: 333px; /* Set a fixed width for the images, adjust as needed */
-                height: 333px; /* Set a fixed height for the images, adjust as needed */
-                object-fit: cover; /* Maintain aspect ratio and cover the entire space */
-            }
-            
-            /* Optional: Center the images vertically if needed */
-            .card img {
-                display: block;
-                margin: 0 auto;
-            }
-
-            .front, .back {
-                position: absolute;
                 width: 100%;
                 height: 100%;
-                backface-visibility: hidden; /* Hide the backface of the card */
+                padding: 0px 0px;
+                color: white;
+                border-radius: 20px;
+                border-radius: 10px 10px 0 0;
             }
 
-            .front {
-                transform: rotateY(0deg); /* Show the front side of the card */
+            .content {
+                line-height: 1.5em;
+                padding: 0px 0px;
+                height: auto;
+                width: 80%;
+                margin-left: 27px;
+                position: relative;
+                background-color: rgba(0, 0, 0, 0.5);
+                backdrop-filter: blur(5px);
+                padding-top: 0;
+                border-radius: 0 0 10px 10px;
+                display: flex;
+                flex-direction: column; /* Make the child elements stack vertically */
+                justify-content: center; /* Center the content vertically */
+                align-items: center; /* Center the content horizontally */
             }
 
-            .back {
-                transform: rotateY(180deg); /* Initially hide the back side of the card */
+            .content h2,
+            h2 {
+                position: relative;
+                font-size: 25px;
                 text-align: center;
-                padding: 20px;
-                background-color: #f1f1f1;
-                color: black; /* Set the text color to black */
-                font-size: 14px; /* Adjust the font size as needed */
-                border-radius: 5px;
+                color: white;
+                border-radius: 10px;
+                padding: 10px;
+                margin-bottom: 0;  
             }
 
-            /* Show the back side of the card on hover */
-            .card:hover .front {
-                transform: rotateY(-180deg);
+            .content h4,
+            h4 {
+                margin-top: 0px;
+                line-height: 2em;
+                font-size: 20px;
+                text-align: center;
+                margin-bottom: 10px;
+                color: white;
+                border-radius: 10px;
+                padding: 0px;
+                font-weight: normal;
+                padding-bottom: 0px;
             }
 
-            .card:hover .back {
-                transform: rotateY(0deg);
+            .content button {
+                display: block;
+                width: 245px;
+                height: 40px;
+                background-color: transparent; 
+                color: #09BA20;
+                border: none;
+                cursor: pointer;
+                font-size: 17px;
+                font-weight: bolder;
+                margin-top: 20px;
+                margin: 0px;
+                text-align: center;
             }
 
-            
-        </style>
+            table {
+                width: 90%;
+                margin-top: 100px;
+                border-collapse: collapse;
+            }
+
+            td {
+                width: 30%; 
+                padding: 5px;
+            }
+            .front {
+                z-index: 2;
+                background-size:100% 100%;
+            }
+            .indoor {
+                background-image: url(images/small-IndoorSystem.jpg);
+            }
+
+            .biggerIndoor {
+                background-image: url(https://i.pinimg.com/564x/7e/26/78/7e267824b2b1e7be78093732af6fb0ea.jpg);
+            }
+
+            .outdoor {
+                background-image: url(https://i.pinimg.com/564x/4e/a2/3c/4ea23c2fc163acdb063ec896b1fbc441.jpg);
+            }
+
+        </style>  
     </head>
     <body>
         <header>
@@ -96,61 +142,45 @@
                 include 'menu.php';
             ?>
         </header>
-        
         <table>
-    <!-- First column -->
-    <tr>
-        <td>
-            <!-- Card 1: Freshwater Indoor System -->
-            <div class="card">
-                <!-- Front of the card -->
-                <div class="front">
-                    <img src="images/indoorSystem1.JPG" alt="freshwater indoor system">
-                </div>
-                <!-- Back of the card -->
-                <div class="back">
-                    <p>Freshwater Indoor System</p>
-                    <p>The Aqua-Bot Freshwater system is a fully automated and web-controlled setup designed for freshwater aquariums and aquaponic systems. Key features include remote control and monitoring through a web application, support for freshwater fish and plants with optimal conditions, automated feeding, aquatic plant management, error notifications, and integration with an aquaponic setup. It offers convenience, automation, and comprehensive control for maintaining a freshwater aquarium, making it an efficient solution for fish and plant enthusiasts.</p>
-                    <button class="more-info-btn">More Info</button>
-                </div>
-            </div>
-        </td>
-
-        <!-- Second column -->
-        <td>
-            <!-- Card 2: Freshwater Outdoor System -->
-            <div class="card">
-                <!-- Front of the card -->
-                <div class="front">
-                    <img src="images/outdoorSystem2.JPG" alt="freshwater outdoor system">
-                </div>
-                <!-- Back of the card -->
-                <div class="back">
-                    <p>Freshwater Outdoor System</p>
-                    <button class="more-info-btn">More Info</button>
-                </div>
-            </div>
-        </td>
-
-        <!-- Third column -->
-        <td>
-            <!-- Card 3: Saltwater Indoor System -->
-            <div class="card">
-                <!-- Front of the card -->
-                <div class="front">
-                    <img src="images/indoorSystem2.JPG" alt="saltwater indoor system">
-                </div>
-                <!-- Back of the card -->
-                <div class="back">
-                    <p>Saltwater Indoor System</p>
-                    <p>The Aqua-Bot Marine system is a fully automated and web-controlled setup designed for marine aquariums. It offers remote control through a web application and ensures optimal conditions for marine fish and coral. With features like automated feeding, coral and reef maintenance, water quality monitoring, error notifications, and additional options like automated water changes and advanced filtration integration, it provides marine enthusiasts with a convenient and comprehensive solution for managing their aquariums.</p>
-                    <button class="more-info-btn">More Info</button>
-                </div>
-            </div>
-        </td>
-    </tr>
-</table>
-
-
+            <tr>
+                <td>
+                    <!-- Woolworths -->
+                    <div class="container" >
+                        <div class="front indoor side">
+                        </div>
+                    </div>
+                    <div class="content">
+                        <h2>INDOOR SYSTEM</h2>
+                        <button>Read More</button>
+                    </div>
+                </td>
+                <td>
+                    <!-- LightsOut -->
+                    <div class="container">
+                        <div class="front biggerIndoor side">
+                        </div>
+                    </div>
+                    <div class="content">
+                        <h2>INDOOR SYSTEM</h2>
+                        <button>Read More</button>
+                    </div>
+                </td>
+                <td>
+                    <!-- Checkers -->
+                    <div class="container" >
+                        <div class="front outdoor side">
+                        </div>
+                    </div>
+                    <div class="content">
+                        <h2>OUTDOOR SYSTEM</h2>
+                        <button>Read More</button>
+                    </div>
+                </td>
+            </tr>
+        </table>
+        <script>
+            
+        </script>
     </body>
 </html>
