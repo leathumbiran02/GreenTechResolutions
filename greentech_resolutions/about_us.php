53e8ff8286b91268cd13c65656765d920bba9f1e
Tonisha <nishagovender0205@gmail.com>
2023-08-06 17:43:31 +0200

    Add diagram and background to aboutUs.php

--- greentech_resolutions/about_us.php
+++ greentech_resolutions/about_us.php
@@ -31,8 +31,13 @@
                 margin: 0;
                 padding: 0;
                 box-sizing: border-box;
+                background-image: url(https://www.pixelstalk.net/wp-content/uploads/images5/Original-Green-Aesthetic-Backgrounds.jpg);
+                background-position:center;
+                background-size:cover;
+                background-attachment: fixed;
+                backdrop-filter: blur(2px);
             }
-
+            
             .container {
                 min-width: 80%;
                 min-height: 420px;
@@ -108,10 +113,13 @@
             .aboutUsFront, .aboutUsBack{
                 background-image: url(https://image.slidesdocs.com/responsive-images/background/green-science-technology-agriculture-farmland-powerpoint-background_d6a0dcbc2c__960_540.jpg);
             }
-            .aquaponicsFront,.aquaponicsBack{
+            .aquaponicsFront{
                 background-image: url(https://worldwaterreserve.com/wp-content/uploads/2018/11/Small-aquaponics-system-on-wooden-table-art.jpg);
             }
-
+            .aquaponicsBack{
+                background-image: url(images/whatIsAquaponics.png);
+            }
+            
             .container button {
                 display: center;
                 align-items: center;
@@ -155,7 +163,7 @@
             }
             table {
                 width: 80%;
-                margin-top: 400px; /* Adjust the margin to move the table lower down */
+                margin-top: 500px; /* Adjust the margin to move the table lower down */
                 border-collapse: collapse;
             }
             td {
@@ -199,17 +207,17 @@
                     <div class="container" id="aquaponics">
                         <div class="front aquaponicsFront side">
                             <div class="content">
-                                <h2>What is Aquaponics?</h2> <!-- Changed from h1 to h2 -->
-                                <h4>Aquaponics is a sustainable farming method that combines aquaculture (fish farming) and hydroponics (growing plants in water) in a symbiotic system. In an aquaponics system, fish waste provides nutrients for the plants, while the plants naturally filter the water for the fish.</h4> <!-- Changed from p to h4 -->
-                                <button onclick="flipCardBack('aquaponics')">Read More</button>
+                                <h2>What is Aquaponics?</h2> 
+                                <h4>Aquaponics is a sustainable farming method that combines aquaculture (fish farming) and hydroponics (growing plants in water) in a symbiotic system. In an aquaponics system, fish waste provides nutrients for the plants, while the plants naturally filter the water for the fish.
+                                The fish waste, which contains ammonia, is broken down by beneficial bacteria into nitrates. These nitrates serve as fertilizer for the plants, allowing them to grow without the need for soil. As the plants absorb the nutrients, they purify the water, creating a clean and healthy environment for the fish.
+                                Its advantages are that it requires significantly less water compared to conventional agriculture, as water is recycled within the system and it eliminates the need for chemical fertilizers and pesticides, promoting organic and sustainable food production.
+                                </h4> 
+                                <button onclick="flipCardBack('aquaponics')">View Process</button>
                             </div>
                         </div>
                         <div class="back aquaponicsBack side" id="aquaponics">
-                            <div class="content">
-                                <h4>The fish waste, which contains ammonia, is broken down by beneficial bacteria into nitrates. These nitrates serve as fertilizer for the plants, allowing them to grow without the need for soil. As the plants absorb the nutrients, they purify the water, creating a clean and healthy environment for the fish.
-
-                                    Its advantages are that it requires significantly less water compared to conventional agriculture, as water is recycled within the system and it eliminates the need for chemical fertilizers and pesticides, promoting organic and sustainable food production. </h4> 
-                                <button onclick="flipCard('aquaponics')">Read Less</button>
+                            <div class="content" style = "background-color: rgba(0, 0, 0, 0); top: 98%;">
+                                <button onclick="flipCard('aquaponics')">Back</button>
                             </div>
                         </div>
                     </div>
