80aabde6bdee8e8ba76d8c591b8bb2023ca157c7
Tonisha <nishagovender0205@gmail.com>
2023-09-05 18:41:21 +0200

    Update login_register.php

--- greentech_resolutions/login_register.php
+++ greentech_resolutions/login_register.php
@@ -10,19 +10,31 @@
         <style>
             body{ /* Adding a background image to the page: */
                 width: 100%;
-                background-image: url(images/login_background.jpg);
-                background-position:center;
-                background-size:cover;
-                background-attachment: fixed;
-                overflow: hidden;
-                margin-top: 80px;
+                /*overflow: hidden; does not allow page to scroll down*/ 
+                margin-top: 20px;
+            }
+            p, span{
+                color: white;
+                font-size: 20px;
+                font-weight: normal;
             }
             p{
-                margin-top: 20px;
-                color: white;
-                font-size: 22px;
-                font-weight: bold;
+                margin-top: 15px;
             }
+            span{
+                margin-top: -10px;
+            }
+            h2{
+                color: white;
+                text-align: center; /* Center the text horizontally */
+                font-size: 20px;
+                font-weight: bolder;
+                padding-bottom: 0px;
+            }
+            .toggle_register_login_btn{
+                margin-left: -27px;
+            }
+
         </style>
     </head>
     <body>
@@ -32,28 +44,25 @@
                 include 'menu.php';
             ?>
         </header>
-        <div class="spacing" style="height:150px;"></div>
         <div class="hero">
             <div class="form_page"> <!-- The body of the page: -->
                 <div class="form-box"> <!-- The login and register form: -->
-                    <div class="register_login_btn_box"> <!-- The buttons that switches between both the login and register form: -->
-                        <div id="btn"></div>
-                        <button type="button" class="toggle_register_login_btn" onclick="login()">Login</button> <!-- When the Login button is clicked call the login() function: -->
-                        <button type="button" class="toggle_register_login_btn" onclick="register()">Sign Up</button> <!-- When the Register button is clicked call the register() function: -->
-                    </div>
-
                     <!--Create a login form for a user to enter their email and password:-->
                     <form class="login_input_group" id="login" name = "login_form" action="user_login.php" method="POST" onsubmit = "return(validate_login());">
+                        <h2>LOGIN</h2>
                         <!-- Email Address: -->
                         <input type = "email" class="input_field" name = "email" placeholder = "Email Address"/>
                         <!-- Password: -->
                         <input type = "password" class="input_field" name = "password" placeholder = "Password"/><br><br>
                         <!-- Button to submit the details to the database: -->
                         <button type="submit" name="login_submit" class="submit_btn">Login</button>
+                        <div style ="text-align: center;">
+                            <span class="account">Don't have an account?</span><button type="button" class="toggle_register_login_btn" onclick="register()">Sign Up</button> <!-- When the Register button is clicked call the register() function: -->
+                        </div>
                     </form>
-
                     <!--Create a registration form that sends the information to the database:-->
                     <form class = "login_input_group" id="register" name = "register_form" action = "register.php" method = "POST" onsubmit = "return(validate_registration());">
+                        <h2>SIGN UP</h2>
                         <!-- Full Name: -->
                         <input type = "text" class="input_field" name = "full_name" placeholder = "Full Name"/>
                         <!-- Email Address: -->
@@ -64,16 +73,21 @@
                         <input type = "password" class="input_field" name = "confirm_pass" placeholder = "Confirm password"/>
                         <!-- BOT IP:-->
                         <input type = "text" class="input_field" name = "bot_ip" placeholder = "BOT IP"/>
-                        <!-- Agree to terms and conditions checkbox -->
-                        <input type="checkbox" class="check_box" id="agree_checkbox"><span class="remember_password">I agree to the terms & conditions</span>
+                        <div style = "margin-top: -20px;">   
+                            <!-- Agree to terms and conditions checkbox -->
+                            <input type="checkbox" class="check_box" id="agree_checkbox"><span>I agree to the terms & conditions<span>
+                        </div>
                         <!--Button to submit the form:-->
-                        <button type="submit" name="register_submit" class="submit_btn">Sign Up</button>       
+                        <button type="submit" name="register_submit" class="submit_btn">Sign Up</button>    
+                        <div style ="text-align: center;">
+                            <span >Already have an account?</span><button type="button" class="toggle_register_login_btn" onclick="login()">Login</button> <!-- When the Login button is clicked call the login() function: -->
+                        </div>
                     </form>
                 </div>
                 <footer style="filter:drop-shadow(1px 1px 3px black);"> <!-- Have a terms and conditions appear at the bottom of the page: -->
-                    <div class="spacing" style="height:50px;"></div>
+                    <div class="spacing" style="height:0px;"></div>
                     <div class="center-text">
-                        <br><p style="font-size: 24px;"><i>Terms & Conditions: By creating an account you agree to share your full name, email address, and Bot IP with GreenTech Resolutions.</i></p>
+                        <br><p style="font-size: 20px;"><i>Terms & Conditions: By creating an account you agree to share your full name, email address, and Bot IP with GreenTech Resolutions.</i></p>
                     </div>
                 </footer>
             </div>
