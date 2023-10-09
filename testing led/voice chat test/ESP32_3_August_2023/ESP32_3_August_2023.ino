20c3289f18fa066d26b6c910c5eabb603fe0e58c
Lea Thumbiran <111317155+leathumbiran02@users.noreply.github.com>
2023-10-09 17:24:49 +0200

    Adjusted esp32 code.

---
+++ testing led/voice chat test/ESP32_3_August_2023/ESP32_3_August_2023.ino
@@ -0,0 +1,105 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+
+#define espSerial Serial2 // Assuming you are using Serial2 for ESP32's TX and RX pins
+
+// Ip address of esp32 on mobile wifi: 192.168.8.114 (check)
+// Ip address of esp32 on home network: 192.168.3.7
+
+const char* ssid = "HUAWEI_E5577_CB36"; /* Wifi name */
+const char* password = "alaykalea1"; /* Wifi password */
+
+AsyncWebServer server(80); /* WebServer port for Apache */
+
+void setup() {
+  Serial.begin(9600);
+  espSerial.begin(9600);
+  delay(1000);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Print the IP address
+  Serial.print("Connected to WiFi. IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Enable CORS - Same Origin Policy
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Route 1 - for most components
+  server.on("/sendCommand", HTTP_POST, [](AsyncWebServerRequest *request) {
+    if (request->hasParam("command", true)) {
+      String command = request->getParam("command", true)->value();
+      sendCommandToArduino(command[0], [&](String response) {
+        // Forward the response to the client (website)
+        request->send(200, "text/plain", response);
+      });
+    } else {
+      request->send(400, "text/plain", "Missing 'command' parameter");
+    }
+  });
+
+  // Route 2 - for water level
+  server.on("/sendAndReceiveWaterLevelCommand", HTTP_POST, [](AsyncWebServerRequest *request) {
+    if (request->hasParam("command", true)) {
+      String command = request->getParam("command", true)->value();
+      sendCommandToArduino(command[0], [&](String response) {
+        // Forward the response (water level) to the client (website)
+        request->send(200, "text/plain", response);
+      });
+    } else {
+      request->send(400, "text/plain", "Missing 'command' parameter");
+    }
+  });
+
+  // Route 3 - for system
+  server.on("/sendAndReceiveCheckSystemCommand", HTTP_POST, [](AsyncWebServerRequest *request) {
+    if (request->hasParam("command", true)) {
+      String command = request->getParam("command", true)->value();
+      sendCommandToArduino(command[0], [&](String response) {
+        // Forward the response (check system) to the client (website)
+        request->send(200, "text/plain", response);
+      });
+    } else {
+      request->send(400, "text/plain", "Missing 'command' parameter");
+    }
+  });
+
+  // Route 4 - for camera
+  // Setup web server route to handle incoming and outgoing commands for camera
+
+  // Route 5 - for temperature
+  server.on("/sendAndReceiveTemperatureCommand", HTTP_POST, [](AsyncWebServerRequest *request) {
+    if (request->hasParam("command", true)) {
+      String command = request->getParam("command", true)->value();
+      sendCommandToArduino(command[0], [&](String response) {
+        // Forward the response (temperature) to the client (website)
+        request->send(200, "text/plain", response);
+      });
+    } else {
+      request->send(400, "text/plain", "Missing 'command' parameter");
+    }
+  });
+
+  // Start web server
+  server.begin();
+}
+
+void loop() {
+  // Your other loop code here (if any)
+}
+
+void sendCommandToArduino(char command, std::function<void(String)> callback) {
+  espSerial.write(command);
+  Serial.println("Command sent to Arduino");
+  // You can call the callback function with the response from Arduino when it becomes available
+  String responseFromArduino = "Response from Arduino"; // Replace with actual response
+  callback(responseFromArduino);
+}
