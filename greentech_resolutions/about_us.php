ed41c7133b2efdf6785c799da6b78dfa48937b5a
Lea Thumbiran <111317155+leathumbiran02@users.noreply.github.com>
2023-10-09 17:16:22 +0200

    Added footer content
    -Social media links for facebook. instagram and youtube.
    -Phone number, address and email added.
    -Links to pages on the site added.
    
    TO DO:
    -Styling and exception handling for the footer.
    -Static map image for the stay connected section.

--- greentech_resolutions/about_us.php
+++ greentech_resolutions/about_us.php
@@ -51,6 +51,9 @@
             .aquaponicsSection{
                 background-color: #4C4D5E;
                 padding-bottom: 20px;
+                margin-left: 50px;
+                margin-right: 50px;
+                margin-bottom: 50px;
             }
             .aquaponicsContent{
                 background-color: #4C4D5E;
@@ -58,12 +61,12 @@
                 padding-left:50px;
                 padding-bottom:20px;
                 padding-top:30px;/* Space in section above text */
-                margin-top:10px;/* Space above section */
+                margin-top:5px;/* Space above section */
             }
             .aquaponicsGallery {
                 display: flex;
                 justify-content: space-between;
-                gap: 1rem; /* Add some gap between images */
+                gap: 0px; /* Add some gap between images */
             }
 
             .aquaponicsGallery img {
@@ -76,6 +79,8 @@
                 width: 35%;
                 max-width: none; /* Remove the max-width restriction to allow bigger width */
                 max-height: none; /* Allow the image to use its natural height */
+                margin-left: 50px;
+                margin-right: 50px;
             }
 
             .aquaponicDiagram {
@@ -107,7 +112,7 @@
                 display: flex;
                 justify-content: space-between;
                 align-items: center;
-                gap: 7rem; /* Add some gap between images */
+                gap: 0rem; /* Add some gap between images */
             }
             .ourTeamFirstCard,
             .ourTeamSecondCard, 
@@ -212,7 +217,7 @@
                 font-weight: normal;
             }
             a {
-                text-decoration: underline;
+                text-decoration: none;
             }
         </style>  
     <body>
@@ -226,47 +231,42 @@
             <img class="personHoldingPlant" src="images/about_us_holding_plant.jpg">
             <div class="aboutUsContent">
                 <h2>ABOUT US</h2>
-                <h4><br>Welcome to GreenTech Resolutions, a company dedicated to
-                sustainability and promoting Eco-friendly practices.
-                We aim to create a greener future using technology, education
-                and, innovation.There is an urgent need to address
-                environmental challenges and strive to lead in sustainable
+                <h4 style="margin-left:50px; margin-right:50px; text-align:justify;  line-height: 1.6;">
+                <br><i>Welcome to GreenTech Resolutions, a company dedicated to
+                sustainability and promoting Eco-friendly practices. We aim to create 
+                a greener future using technology, education
+                and innovation. There is an urgent need to address
+                environmental challenges and a strive to lead in sustainable
                 solutions.
                 <br> <br> 
                 Our mission is to educate and empower individuals about
-                sustainable living and provide them with the tools and knowledge
+                sustainable living and to provide them with the tools and knowledge
                 to make a positive impact. We offer aquaponics systems that
                 integrate robotics and automation, providing a seamless and
                 user-friendly experience. Our technology allows for full
-                automation and a website interface lets users monitor and
+                automation and a website interface that lets users monitor and
                 control their aquaponics system.
-                </h4>
+                </i></h4>
             </div>
         </div>
         <div class="aquaponicsSection">
             <div class="aquaponicsContent">
-                <h2>WHAT IS AQUAPONICS</h2>
-                <h4><br>Aquaponics is a sustainable farming method that combines
+                <h2>WHAT IS AQUAPONICS?</h2>
+                <h4 style="margin-left:50px; margin-right:50px; text-align:justify;  line-height: 1.6;"><i><br>Aquaponics is a sustainable farming method that combines
                 aquaculture (fish farming) and hydroponics (growing plants in
-                water) in a symbiotic system. In an aquaponics system, fish
+                water) in a symbiotic system.
+                In an aquaponics system, fish
                 waste provides nutrients for the plants, while the plants
-                naturally filter the water for the fish.
-                <br><br>
-                Aquaponics is a sustainable farming method that combines
-                aquaculture (fish farming) and hydroponics (growing plants in
-                water) in a symbiotic system. In an aquaponics system, fish
-                waste provides nutrients for the plants, while the plants
-                naturally filter the water for the fish. The fish waste, which
-                contains ammonia, is broken down by beneficial bacteria into
+                naturally filter the water for the fish. The fish waste, which contains ammonia, is broken down by beneficial bacteria into
                 nitrates. These nitrates serve as fertilizer for the plants,
                 allowing them to grow without the need for soil. As the plants
                 absorb the nutrients, they purify the water, creating a clean
-                and healthy environment for the fish. Its advantages are that it
+                and healthy environment for the fish. <br><br> Its advantages are that it
                 requires significantly less water compared to conventional
-                agriculture, as water is recycled within the system and it
-                eliminates the need for chemical fertilizers and pesticides,
-                promoting organic and sustainable food production.
-            </h4>
+                agriculture as the water is recycled within the system and it
+                eliminates the need for chemical fertilizers and pesticides. This
+                promotes organic and sustainable food production.
+            </i></h4>
             </div>  
         </div>
         <div class="aquaponicsGallery">
@@ -275,33 +275,33 @@
             <img class="galleryImg2" src="images/aquaponicsGallery2.JPG" >
         </div>
         <div class="ourTeamSection">
-            <h2>OUT TEAM</h2>
+            <h2>OUR TEAM</h2>
             <div class="ourTeamContent">
                 <div class="ourTeamFirstCard">
                     <img class="ourPictures" src="images/mackylesPic.png" >
                     <h2><br>MACKYLE NAIDOO</h2>
                     <h4>BSc IT Robotics</h4>
-                    <a href="https://www.linkedin.com/in/mackyle-naidoo-15552723b/"><br>Linked in</a>
+                    <a href="https://www.linkedin.com/in/mackyle-naidoo-15552723b/"><br>LinkedIn</a>
                     <a href="https://github.com/Kyle1802"><br>GitHub</a>
                 </div>
                 <div class="ourTeamSecondCard">
                     <img class="ourPictures" src="images/leasPic.png" >
                     <h2><br>LEA THUMBIRAN</h2>
                     <h4>BSc IT Software Engineering</h4>
-                    <a href="https://www.linkedin.com/in/lea-t-42b352211/"><br>Linked in</a>
+                    <a href="https://www.linkedin.com/in/lea-t-42b352211/"><br>LinkedIn</a>
                     <a href="https://github.com/leathumbiran02"><br>GitHub</a>
                 </div>
                 <div class="ourTeamThirdCard">
                     <img class="ourPictures" src="images/toniPic.jpg" >
                     <h2><br>TONISHA GOVENDER</h2>
                     <h4>BSc IT Software Engineering</h4>
-                    <a href="https://www.linkedin.com/in/tonisha-govender-584785219/"><br>Linked in</a>
+                    <a href="https://www.linkedin.com/in/tonisha-govender-584785219/"><br>LinkedIn</a>
                     <a href="https://github.com/TonishaGovender"><br>GitHub</a>
                 </div>
             </div>
         </div>
         <div class="chooseUsSection">
-            <h2>WHY CHOOSE US<br></h2>
+            <h2>WHY CHOOSE US?<br></h2>
             <div class="chooseUsContent">
                 <div class="chooseUsFirstRow">
                     <div class="chooseUsFirstCard">
--- greentech_resolutions/footer.php
+++ greentech_resolutions/footer.php
@@ -1,12 +1,15 @@
 <html>
     <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <style>
             .block{
                 background-color: #4C4D5E;
-                width: 100%;
+                width: 95%;
                 height: 200px;
                 margin-bottom: 50px;
                 display: flex;
+                margin-left: 50px;
+                margin-right: 50px;
             }
             .logo{
                 height: 80px;
@@ -17,24 +20,117 @@
             .info {
                 display: flex;
                 flex-direction: column;
-                margin-left: 80px; 
-                margin-top: 50px;
+                margin-left: 200px; 
+                margin-top: 20px;
+                line-height: 1.6;
             }
-            span{
+            span.span_footer{
                 color: white;
                 float: left;
                 font-size: 20px;
+            }
+            .fa.fa-instagram {
+                color: transparent;
+                background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%);
+                background: -webkit-radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%);
+                background-clip: text;
+                -webkit-background-clip: text;
+            }
+            .fa {
+                padding: 5px;
+                font-size: 30px;
+                width: 40px;
+                text-align: center;
+                text-decoration: none;
+                border-radius: 50%;
+            }
+            .fa:hover {
+                opacity: 0.7;
+            }
+            .fa.fa-facebook-f{
+                background: #3B5998;
+                color: white;
+            }
+            .fa.fa-youtube-play{
+                color: white;
+                background-color: white;
+            }
+            .list{
+                display: flex;
+                flex-direction: row;
+                gap:100px;
+
             }
         </style>
     </head>
     <body>
         <div class="block">
-            <img class="logo" src="images/gtr-logo2.png" alt="GreenTechResolutions" style="margin-top: 55px; padding-left: 50px;">
+            <!-- Adding a logo that navigates to the plant page when logged in: -->
+            <?php if(isset($_SESSION['users']) && $_SESSION['users'] == true): ?>
+                <a href="plant.php"><img class="logo" src="images/gtr-logo2.png" alt="GreenTechResolutions" style="margin-top: 55px; padding-left: 50px;"></a>
+            <?php else: ?> <!-- Logo to navigate to the about us page when not logged in: -->
+                <a href="about_us.php"><img class="logo" src="images/gtr-logo2.png" alt="GreenTechResolutions" style="margin-top: 55px; padding-left: 50px;"></a>
+            <?php endif; ?>
+
             <div class="info">
-                <span>9 Concorde E Rd, Bedfordview JHB</span><br>  
-                <span>+27 74 614 9038</span><br> 
-                <span>greentechresolutions@gmail.com</span><br> 
+                <span class="span_footer" style="margin-left:70px;">STAY CONNECTED</span>
+                <span class="span_footer"><a href="https://goo.gl/maps/UNgsTJUgLagswkQE6">9 Concorde E Rd, Bedfordview JHB</a></span><br>  
+                <span class="span_footer"><a href="tel:+27746149038">+27 74 614 9038</a></span><br> 
+                <span class="span_footer"><a href="mailto:greentechresolutions@gmail.com">greentechresolutions@gmail.com</a></span><br> 
+            </div>
+            <div class="info">
+                <span class="span_footer" style="margin-left:-13px;">SOCIAL</span>
+                <span class="span_footer"><a href="https://www.youtube.com/@GreenTechResolutions/featured"><i class="fa fa-youtube-play" style="color:#FF0000;"></i></a></span><br>  
+                <span class="span_footer"><a href="https://www.instagram.com/greentechresolutions/"><i class="fa fa-instagram"></i></a></span><br> 
+                <span class="span_footer"><a href="https://www.facebook.com/green.tech.resolutions?mibextid=ZbWKwL"><i class="fa fa-facebook-f" ></i></a></span><br>
+            </div>
+            <div class="info" style="margin-top:20px;">
+                <span class="span_footer">GREEN TECH RESOLUTIONS</span>
+                <nav class="navbar">
+                    <ul class="list" style="list-style-type: none;">
+                        <!-- Only display this menu if the person logged in is a user: -->
+                        <?php if(isset($_SESSION['users']) && $_SESSION['users'] == true): ?>
+                            <li><a>AquaBot</a>
+                                <ul style="list-style-type: none;">
+                                    <li><a href="plant.php">Plants</a></li>
+                                    <li><a href="tank.php">Tank</a></li>
+                                    <li><a href="system.php">System</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="shop.php">Shop</a></li>
+                            <li><a>Connect</a>
+                                <ul style="list-style-type: none;">
+                                    <li><a href="chat.php">With Us</a></li>
+                                    <li><a href="community.php">Community</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="learn.php">Learn</a></li>
+                            <li><a href="account.php">Account</a>
+                                <ul style="list-style-type: none;">
+                                    <li><a href="logout.php" onclick="return confirm('Are you sure that you want to logout?')">Logout</a></li>
+                                </ul>
+                            </li>
+                        
+                            <!-- Microphone button for users to talk to the system: -->
+                            <button id="microphoneButton" class='microphone' style="font-size:30px;"><i class="fas fa-microphone"></i></button>
+                        <?php else: ?> <!-- Display this menu for guest users only: -->
+                            <div id="list1">
+                                <li><a href="about_us.php">About Us</a></li>
+                                <li><a href="partners.php">Partners</a></li>
+                                <li><a href="products.php">Products</a></li>
+                                <!-- <li><a href="contact_us.php">Contact Us</a></li>
+                                <li><a href="login_register.php">Connect Bot</a></li> -->
+                            </div>
+                            <div id="list2">
+                                <li><a href="contact_us.php">Contact Us</a></li>
+                                <li><a href="login_register.php">Connect Bot</a></li>
+                            </div>
+                        <?php endif; ?>
+                    </ul>
+                </nav>
             </div>
         </div>
     </body>
-<html>
+    <!-- Provide the link for the JQuery Library: -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</html>
