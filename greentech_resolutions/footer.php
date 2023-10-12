86d18c38504f77a149810518ee0621c38f92e387
Tonisha <nishagovender0205@gmail.com>
2023-10-12 22:23:17 +0200

    Styled footer

--- greentech_resolutions/footer.php
+++ greentech_resolutions/footer.php
@@ -4,37 +4,50 @@
         <style>
             .block{
                 background-color: #4C4D5E;
-                width: 95%;
+                width: 93%;
                 height: 200px;
                 margin-bottom: 50px;
                 display: flex;
                 margin-left: 50px;
                 margin-right: 50px;
             }
-            .logo{
-                height: 80px;
+            .footerLogo{
+                height: 110px;
                 width: auto;
-                /* padding-left: 50px; */
-                /* margin-top: 55px; */
+                padding-left: 50px; 
+                margin-top: 40px;
             }
             .info {
                 display: flex;
                 flex-direction: column;
-                margin-left: 200px; 
-                margin-top: 20px;
-                line-height: 1.6;
+                margin-left: 130px; 
+                width: 250px;
+                margin-top: 40px;
             }
-            span.span_footer{
+            span{
                 color: white;
                 float: left;
+                margin-left: -90px;
                 font-size: 20px;
             }
-            .fa.fa-instagram {
-                color: transparent;
-                background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%);
-                background: -webkit-radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%);
-                background-clip: text;
-                -webkit-background-clip: text;
+            .instalLogo{
+                width: 50px;
+            }
+            .facebookLogo{
+                width: 50px;
+                margin-left: 15px;
+            }
+            .youtubeLogo{
+                width: 75px;
+            }
+            .socialMedia{
+                margin-top: 70px;
+                margin-left: -100px;
+            }
+            .menuList{
+                width: 250px;
+                margin-top: -10px;
+                margin-left: -100px;
             }
             .fa {
                 padding: 5px;
@@ -47,19 +60,17 @@
             .fa:hover {
                 opacity: 0.7;
             }
-            .fa.fa-facebook-f{
-                background: #3B5998;
-                color: white;
-            }
-            .fa.fa-youtube-play{
-                color: white;
-                background-color: white;
-            }
             .list{
                 display: flex;
                 flex-direction: row;
                 gap:100px;
-
+            }
+            li{
+                padding-top: 10px;
+                text-align: center;
+            }
+            li a:hover{
+                color: green;
             }
         </style>
     </head>
@@ -69,23 +80,16 @@
             <?php if(isset($_SESSION['users']) && $_SESSION['users'] == true): ?>
                 <a href="plant.php"><img class="logo" src="images/gtr-logo2.png" alt="GreenTechResolutions" style="margin-top: 55px; padding-left: 50px;"></a>
             <?php else: ?> <!-- Logo to navigate to the about us page when not logged in: -->
-                <a href="about_us.php"><img class="logo" src="images/gtr-logo2.png" alt="GreenTechResolutions" style="margin-top: 55px; padding-left: 50px;"></a>
+                <a href="about_us.php"><img class="footerLogo" src="images/gtr-logo2.png" alt="GreenTechResolutions"></a>
             <?php endif; ?>
 
             <div class="info">
-                <span class="span_footer" style="margin-left:70px;">STAY CONNECTED</span>
-                <span class="span_footer"><a href="https://goo.gl/maps/UNgsTJUgLagswkQE6">9 Concorde E Rd, Bedfordview JHB</a></span><br>  
-                <span class="span_footer"><a href="tel:+27746149038">+27 74 614 9038</a></span><br> 
-                <span class="span_footer"><a href="mailto:greentechresolutions@gmail.com">greentechresolutions@gmail.com</a></span><br> 
+                <span><a href="https://goo.gl/maps/UNgsTJUgLagswkQE6">9 Concorde E Rd, Bedfordview JHB</a></span><br>  
+                <span><a href="tel:+27746149038">+27 74 614 9038</a></span><br> 
+                <span><a href="mailto:greentechresolutions@gmail.com">greentechresolutions@gmail.com</a></span><br> 
             </div>
-            <div class="info">
-                <span class="span_footer" style="margin-left:-13px;">SOCIAL</span>
-                <span class="span_footer"><a href="https://www.youtube.com/@GreenTechResolutions/featured"><i class="fa fa-youtube-play" style="color:#FF0000;"></i></a></span><br>  
-                <span class="span_footer"><a href="https://www.instagram.com/greentechresolutions/"><i class="fa fa-instagram"></i></a></span><br> 
-                <span class="span_footer"><a href="https://www.facebook.com/green.tech.resolutions?mibextid=ZbWKwL"><i class="fa fa-facebook-f" ></i></a></span><br>
-            </div>
+            
             <div class="info" style="margin-top:20px;">
-                <span class="span_footer">GREEN TECH RESOLUTIONS</span>
                 <nav class="navbar">
                     <ul class="list" style="list-style-type: none;">
                         <!-- Only display this menu if the person logged in is a user: -->
@@ -113,15 +117,12 @@
                         
                             <!-- Microphone button for users to talk to the system: -->
                             <button id="microphoneButton" class='microphone' style="font-size:30px;"><i class="fas fa-microphone"></i></button>
-                        <?php else: ?> <!-- Display this menu for guest users only: -->
-                            <div id="list1">
+                        
+                            <?php else: ?> <!-- Display this menu for guest users only: -->
+                            <div class = "menuList">
                                 <li><a href="about_us.php">About Us</a></li>
                                 <li><a href="partners.php">Partners</a></li>
                                 <li><a href="products.php">Products</a></li>
-                                <!-- <li><a href="contact_us.php">Contact Us</a></li>
-                                <li><a href="login_register.php">Connect Bot</a></li> -->
-                            </div>
-                            <div id="list2">
                                 <li><a href="contact_us.php">Contact Us</a></li>
                                 <li><a href="login_register.php">Connect Bot</a></li>
                             </div>
@@ -129,6 +130,13 @@
                     </ul>
                 </nav>
             </div>
+
+            <div class="socialMedia">
+                <a href="https://www.youtube.com/@GreenTechResolutions/featured"><img class="youtubeLogo" src="images/logoYoutube.png"></a>
+                <a href="https://www.instagram.com/greentechresolutions/"><img class="instalLogo" src="images/logoInstagram.png"></a>
+                <a href="https://www.facebook.com/green.tech.resolutions?mibextid=ZbWKwL"><img class="facebookLogo" src="images/logoFacebook.png"></a>
+            </div>
+
         </div>
     </body>
     <!-- Provide the link for the JQuery Library: -->
