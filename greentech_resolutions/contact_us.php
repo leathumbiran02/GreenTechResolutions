d80099999462320bbeb3f45278d1952c2e370652
Tonisha <nishagovender0205@gmail.com>
2023-10-16 11:52:55 +0200

    Style contactUs.php

--- greentech_resolutions/contact_us.php
+++ greentech_resolutions/contact_us.php
@@ -11,26 +11,86 @@
         <!--Using a CSS style sheet for the page-->
         <link rel="stylesheet" href="style.css">
         <style>
-            .hero{ 
-                backdrop-filter: blur(5px);
+            .contactUsContainer{
+                display: flex;
+                width: 92%;
+                height: auto;
+                justify-content: space-between;
+                margin: 0px 50px;
+                margin-top: -20px;
+                text-align: center;
             }
-            body{ /* Adding a background image to the page: */
-                width: 100%;
-                background-image: url(images/contact_us_background.jpg);
-                background-position:center;
-                background-size:cover;
-                background-attachment: fixed;
-                overflow: hidden;
-                margin-top: 80px;
+            p, span{
+                color: white;
+                font-size: 20px;
+                font-weight: normal;
             }
             p{
-                margin-top: 20px;
-                color: black;
-                font-size: 22px;
-                font-weight: bold;
+                margin-top: 15px;
             }
-            a{
-                color: #62f875;
+            span{
+                margin-top: -10px;
+                color: white;
+            }
+            h2{
+                color: white;
+                text-align: center; /* Center the text horizontally */
+                font-size: 20px;
+                font-weight: bolder;
+                padding-bottom: 0px;
+            }
+            
+            .addressCard {
+                display: flex;
+                flex-direction: column;
+                align-items: left;
+                text-align: left;
+                padding-left: 50px;
+            }
+
+            .addressItem {
+                margin: 10px 0;
+                text-align: left !important;
+            }
+
+            .addressItem a {
+                color: white;
+                font-size: 20px;
+                text-decoration: none;
+                margin-left: 0px;
+                padding-left: -1000px;
+            }
+            .contact-us-form-box {
+                background: none;
+                border: none;
+                border-radius: 0;
+                margin: 0;
+                padding: 0;
+            }
+
+            .contact_us_submit_btn {
+                width: 45%;
+                padding: 5px 0;
+                cursor: pointer;
+                display: inline-block;
+                margin: 40px 0; /* Fix the margin property here */
+                background-color: #009414;
+                color: #ffffff;
+                font-weight: normal;
+                font-size: 20px;
+                border: 0;
+                outline: none;
+                margin-top: 10px;
+                text-align: center; /* Center the text horizontally within the button */
+            }
+
+            .contact-us-login_input_group{
+                margin-top: -110px;
+                padding: 0px;
+            }
+            .map{
+                width: 600px;
+                height: auto;
             }
         </style>
     </head>
@@ -41,37 +101,39 @@
                 include 'menu.php';
             ?>
         </header>
-        <div class="hero">
-            <div class="form_page"> <!-- The body of the page: -->
-                <div class="center-text">
-                    <h1 class="contact-us-toggle_register_login_btn" style="color:white; filter:drop-shadow(1px 1px 3px black);">Contact Us:</h1>
-                    <h3 class="contact-us-toggle_register_login_btn" style="color:white; filter:drop-shadow(1px 1px 3px black);">Head Office Address: <a href="https://goo.gl/maps/UNgsTJUgLagswkQE6">9 Concorde E Rd, Bedfordview Johannesburg</a><br><br> Phone Number: <a href="tel:+27746149038">+27 74 614 9038</a><br><br> Email Address: <a href="mailto:greentechresolutions@gmail.com">greentechresolutions@gmail.com</a><br></h3>
+        <div class="contact-us-register_login_btn_box">
+            <button type="button" class="contact-us-toggle_register_login_btn"><h2>CONTACT US<h2></button> 
+        </div>
+        <div class="contactUsContainer"> <!-- The body of the page: -->
+            <div class="addressCard">
+                <div class="mapCard">
+                    <img class="map" src="images/map.JPG">
                 </div>
-
-
-                <div class="contact-us-form-box"> 
-                    <div class="contact-us-register_login_btn_box">
-                        <button type="button" class="contact-us-toggle_register_login_btn">Leave A Comment</button> 
+                <div class="addressInfo">
+                    <div class="addressItem">
+                        <a href="https://goo.gl/maps/UNgsTJUgLagswkQE6">9 Concorde E Rd, Bedfordview Johannesburg</a>
                     </div>
-
-                    <form class="contact-us-login_input_group" id="contact_us" name = "contact_us" action="contact_us_send_email.php" method="POST" onsubmit = "return(validate_contact_us());">
-                        <!-- Full Name: -->
-                        <input type = "text" class="contact-us-input_field" name = "full_name" placeholder = "Full Name"/>
-                        <!-- Email Address: -->
-                        <input type = "email" class="contact-us-input_field" name = "email" placeholder = "Email Address"/>
-                        <!-- Comments: -->
-                        <input type = "text" class="contact-us-input_field" name = "comments" placeholder = "Comments"/><br><br>
-                        <!-- Button to submit the details to the database: -->
-                        <button type="submit" name="contact_us_submit" class="contact-us-submit_btn">Confirm</button>
-                    </form>
+                    <div class="addressItem">
+                        <a href="tel:+27746149038">+27 74 614 9038</a>
+                    </div>
+                    <div class="addressItem">
+                        <a href="mailto:greentechresolutions@gmail.com">greentechresolutions@gmail.com</a>
+                    </div>
                 </div>
-                <footer style="filter:drop-shadow(1px 1px 3px black); margin-top:-50px;"> <!-- Have a notice appear at the bottom of the page: -->
-                    <div class="spacing" style="height:130px;"></div>
-                    <div class="center-text">
-                        <br><p style="color:white; font-size: 24px;"><i>Notice: All requests will be handled during working hours (8am-5pm) from Monday to Friday.</i></p>
-                    </div>
-                </footer>
             </div>
+
+            <div class="contact-us-form-box"> 
+                <form class="contact-us-login_input_group" id="contact_us" name = "contact_us" action="contact_us_send_email.php" method="POST" onsubmit = "return(validate_contact_us());">
+                    <!-- Full Name: -->
+                    <input type = "text" class="input_field" name = "full_name" placeholder = "Full Name"/><br>
+                    <!-- Email Address: -->
+                    <input type = "email" class="input_field" name = "email" placeholder = "Email Address"/><br>
+                    <!-- Comments: -->
+                    <input type = "text" class="input_field" name = "comments" placeholder = "Comments"/><br>
+                    <!-- Button to submit the details to the database: -->
+                    <button type="submit" name="contact_us_submit" class="contact_us_submit_btn">Confirm</button>
+                </form>
+            </div>            
         </div>
         <!--Use an external javascript file named validate.js to validate the form:-->
         <script src="validate.js"></script>
