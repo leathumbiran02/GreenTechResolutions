82f24233ddd68c4224ec091d492a2b4e33c28beb
Lea Thumbiran <111317155+leathumbiran02@users.noreply.github.com>
2023-10-26 10:47:26 +0200

    Added new image scripts

--- Documentation/Database Scripts/fish_details.sql
+++ Documentation/Database Scripts/fish_details.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2023 at 10:04 PM
--- Server version: 8.0.28
+-- Generation Time: Oct 26, 2023 at 10:43 AM
+-- Server version: 8.0.30
 -- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -41,16 +41,16 @@ CREATE TABLE `fish_details` (
 --
 
 INSERT INTO `fish_details` (`fish_details_id`, `fish_name`, `description`, `image`, `health_benefits`, `recipe`) VALUES
-(1, 'Koi', 'Koi fish are elegant and colorful freshwater fish often associated with beauty and perseverance.', 'https://th.bing.com/th/id/R.9da8b6d7cb62860a50e18dacb6ab3de6?rik=8YimSHqNGIE2JA&pid=ImgRaw&r=0', 'https://www.americanoceans.org/facts/can-you-eat-koi-fish/#:~:text=Koi%20fish%20have%20a%20rich,most%20parts%20of%20the%20world.', 'https://cookpad.com/us/search/koi'),
-(2, 'Trout', 'Trout are freshwater fish known for their distinctive speckled appearance and delicious taste.', 'https://th.bing.com/th/id/R.bbe78ccf12cfc4b596982d121a07fe46?rik=T3nuBVyjpvj%2f3g&riu=http%3a%2f%2fwww.animalspot.net%2fwp-content%2fuploads%2f2012%2f11%2fBrook-Trout-Pictures.jpg&ehk=iY%2fVNpmsXWsr1C%2bjnnFoVDvN4Pe0HX%2b6iOa0hlt3Eac%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1', 'https://www.nutritionadvance.com/rainbow-trout-nutrition-benefits/', 'https://www.foodandwine.com/seafood/fish/trout/trout-recipes'),
-(3, 'Salmon', 'Salmon is a nutritious and flavorful fish prized for its pink flesh and rich omega-3 fatty acids.', 'https://th.bing.com/th/id/R.f1316fa05853225245edd0b1f611cf79?rik=u%2fYbFo%2bVp%2bbHdg&pid=ImgRaw&r=0', 'https://www.healthline.com/nutrition/salmon-nutrition-and-health-benefits#4', 'https://www.delish.com/cooking/recipe-ideas/g2039/salmon-recipes/'),
-(4, 'Pike', 'Pike is a species of freshwater fish known for its distinctive markings and aggressive behaviour.', 'https://th.bing.com/th/id/R.484ec6c0be245379bff2e988cdef5165?rik=ZwTeP%2f2gPEqOlA&pid=ImgRaw&r=0', 'https://www.webmd.com/diet/what-are-health-benefits-pike#:~:text=As%20a%20high%2Dprotein%20food,your%20bones%20and%20muscles%20strong.', 'https://cookingchew.com/pike-recipes.html'),
-(5, 'Bluegill', 'Bluegill is a freshwater fish species native to North America.', 'https://th.bing.com/th/id/OIP._6jI3q05-BYUr0UmWebjLAHaE2?pid=ImgDet&rs=1', 'https://healthfully.com/543501-nutritional-value-of-bluegills.html', 'https://www.tasteofhome.com/recipes/bluegill-parmesan/'),
-(6, 'Perch', 'Perch is a freshwater fish known for its small size and distinctive spiny dorsal fin.', 'https://th.bing.com/th/id/R.b7cc79e498dc97de277303a987375c10?rik=bIBW1bXJyiAofg&pid=ImgRaw&r=0', 'https://toplist.info/top-list/health-benefits-of-eating-perch-9085.htm', 'https://www.yummly.com/recipes/perch-fish'),
-(7, 'Catfish', 'Catfish are freshwater fish known for their distinctive whisker-like barbels and often having a bottom-dwelling behavior.', 'https://th.bing.com/th/id/OIP.5LujS28-cbi31sNpO4vaqgHaE5?pid=ImgDet&rs=1', 'https://www.healthline.com/nutrition/is-catfish-healthy#:~:text=Catfish%20is%20low%20in%20calories,methods%20like%20baking%20or%20broiling.', 'https://www.tasteofhome.com/collection/catfish-recipes/'),
-(8, 'Walleye', 'Walleye is a popular freshwater fish known for its delicious taste and distinctive large, glassy eyes.', 'https://th.bing.com/th/id/OIP.4Uu0MRsC_q9BBNjqZzryJgHaEK?pid=ImgDet&rs=1', 'https://www.nutrition-and-you.com/walleye-fish.html', 'https://insanelygoodrecipes.com/walleye-recipes/'),
-(9, 'Bass', 'Bass fish are known for their strong fighting spirit and delicious flavour.', 'https://th.bing.com/th/id/OIP.cMX4stKPkldmj61tP7Yl6AHaE-?pid=ImgDet&rs=1', 'https://www.verywellfit.com/bass-fish-nutrition-facts-and-health-benefits-5207335#:~:text=Among%20the%20list%20of%20health,and%20combats%20anxiety%20and%20depression.', 'https://insanelygoodrecipes.com/bass-recipes/'),
-(10, 'Carp', 'Carp are freshwater fish known for their distinctive scales and bottom-feeding habits.', 'https://th.bing.com/th/id/OIP._r4OqD_WZU_YTVrnjWkJngHaE8?pid=ImgDet&rs=1', 'https://www.nutrition-and-you.com/common-carp.html', 'https://cookpad.com/us/search/carp');
+(1, 'Koi', 'Koi fish are elegant and colorful freshwater fish often associated with beauty and perseverance.', 'https://i.pinimg.com/originals/3a/08/aa/3a08aa8caf683a6bf2c1b27b60ca3e15.jpg', 'https://www.americanoceans.org/facts/can-you-eat-koi-fish/#:~:text=Koi%20fish%20have%20a%20rich,most%20parts%20of%20the%20world.', 'https://cookpad.com/us/search/koi'),
+(2, 'Trout', 'Trout are freshwater fish known for their distinctive speckled appearance and delicious taste.', 'https://i.pinimg.com/originals/27/a2/8a/27a28ad1c589b9cd379b76a8ae9599cb.jpg', 'https://www.nutritionadvance.com/rainbow-trout-nutrition-benefits/', 'https://www.foodandwine.com/seafood/fish/trout/trout-recipes'),
+(3, 'Salmon', 'Salmon is a nutritious and flavorful fish prized for its pink flesh and rich omega-3 fatty acids.', 'https://i.pinimg.com/originals/09/7b/20/097b20e86fc1098876af8952fc28eee9.jpg', 'https://www.healthline.com/nutrition/salmon-nutrition-and-health-benefits#4', 'https://www.delish.com/cooking/recipe-ideas/g2039/salmon-recipes/'),
+(4, 'Pike', 'Pike is a species of freshwater fish known for its distinctive markings and aggressive behaviour.', 'https://i.pinimg.com/originals/c2/f3/30/c2f33053b8c654fc4aaca4877d8e4d7b.jpg', 'https://www.webmd.com/diet/what-are-health-benefits-pike#:~:text=As%20a%20high%2Dprotein%20food,your%20bones%20and%20muscles%20strong.', 'https://cookingchew.com/pike-recipes.html'),
+(5, 'Bluegill', 'Bluegill is a freshwater fish species native to North America.', 'https://i.pinimg.com/originals/6c/fc/9f/6cfc9f5222d063eece4627f78db4b978.jpg', 'https://healthfully.com/543501-nutritional-value-of-bluegills.html', 'https://www.tasteofhome.com/recipes/bluegill-parmesan/'),
+(6, 'Perch', 'Perch is a freshwater fish known for its small size and distinctive spiny dorsal fin.', 'https://i.pinimg.com/originals/2f/e9/01/2fe90170c191d93303ca185300bf3eba.jpg\r\n', 'https://toplist.info/top-list/health-benefits-of-eating-perch-9085.htm', 'https://www.yummly.com/recipes/perch-fish'),
+(7, 'Catfish', 'Catfish are freshwater fish known for their distinctive whisker-like barbels and often having a bottom-dwelling behavior.', 'https://i.pinimg.com/originals/0b/38/f2/0b38f29e018389c0a71a1525f6e2a795.jpg\r\n', 'https://www.healthline.com/nutrition/is-catfish-healthy#:~:text=Catfish%20is%20low%20in%20calories,methods%20like%20baking%20or%20broiling.', 'https://www.tasteofhome.com/collection/catfish-recipes/'),
+(8, 'Walleye', 'Walleye is a popular freshwater fish known for its delicious taste and distinctive large, glassy eyes.', 'https://i.pinimg.com/originals/51/9d/2b/519d2b75aeaa1053caf0c05b0aba7d92.jpg\r\n', 'https://www.nutrition-and-you.com/walleye-fish.html', 'https://insanelygoodrecipes.com/walleye-recipes/'),
+(9, 'Bass', 'Bass fish are known for their strong fighting spirit and delicious flavour.', 'https://i.pinimg.com/originals/75/de/22/75de226b523a73107af3a41921afe05e.jpg\r\n', 'https://www.verywellfit.com/bass-fish-nutrition-facts-and-health-benefits-5207335#:~:text=Among%20the%20list%20of%20health,and%20combats%20anxiety%20and%20depression.', 'https://insanelygoodrecipes.com/bass-recipes/'),
+(10, 'Carp', 'Carp are freshwater fish known for their distinctive scales and bottom-feeding habits.', 'https://i.pinimg.com/originals/7d/79/82/7d7982efaa407018bb6662946c15dbad.jpg\r\n', 'https://www.nutrition-and-you.com/common-carp.html', 'https://cookpad.com/us/search/carp');
 
 --
 -- Indexes for dumped tables
--- Documentation/Database Scripts/plant_details.sql
+++ Documentation/Database Scripts/plant_details.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 28, 2023 at 03:36 PM
+-- Generation Time: Oct 26, 2023 at 10:47 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.2.0
 
@@ -40,15 +40,15 @@ CREATE TABLE `plant_details` (
 -- Dumping data for table `plant_details`
 --
 
-INSERT INTO plant_details (plant_details_id, plant_name, description, image, health_benefits, recipe) VALUES
-(1, 'Tomato', 'Tomato is a versatile and delicious fruit commonly used in various culinary dishes.', 'https://th.bing.com/th/id/OIP.NNiG9NKcVyJ8fPh66V3WSgHaFW?pid=ImgDet&rs=1', 'https://www.health.com/nutrition/health-benefits-tomatoes', 'https://www.delish.com/cooking/g1448/quick-easy-tomato-recipes/'),
-(2, 'Cauliflower', 'Cauliflower is a nutritious vegetable with a mild, versatile flavor.', 'https://th.bing.com/th/id/R.a9bb5f4e2784a4e038cbae92634cef10?rik=WkEZnPmHt7FiNQ&riu=http%3a%2f%2fnutrawiki.org%2fwp-content%2fuploads%2f2015%2f09%2fCauliflower.jpg&ehk=McP9NlaloU%2bdei9SFVtIx%2fNOFu1Xsc1nnFpasDVm4xI%3d&risl=&pid=ImgRaw&r=0', 'https://www.medicalnewstoday.com/articles/282844', 'https://www.delish.com/cooking/recipe-ideas/g2861/cauliflower-recipes/'),
-(3, 'Spinach', 'Spinach is a nutritious leafy green vegetable packed with vitamins and minerals.', 'https://cdn.mos.cms.futurecdn.net/atyrpYQoxdoTzmEgu8HMWE-1200-80.jpg', 'https://pharmeasy.in/blog/15-reasons-why-spinach-is-called-a-superfood/', 'https://www.delish.com/cooking/g2013/spinach/'),
-(4, 'Carrot', 'The carrot is a nutritious orange root vegetable.', 'https://th.bing.com/th/id/R.2f44d15737bd7c1f6bd5d843ec289879?rik=MGTrOg5HZ5jVCQ&pid=ImgRaw&r=0', 'https://www.health.com/nutrition/health-benefits-of-carrots#:~:text=Carrots%20are%20full%20of%20benefits,this%20vegetable%20into%20your%20diet.', 'https://www.delish.com/cooking/g929/carrot-recipes/'),
-(5, 'Broccoli', 'Broccoli is a nutritious green vegetable.', 'https://th.bing.com/th/id/R.60331a2e67ae8eadeeba0cd68363d114?rik=ZwcO9IoH6suCyw&riu=http%3a%2f%2f2.bp.blogspot.com%2f-jWeRRnyX-VE%2fWQX674gHsLI%2fAAAAAAAABkU%2ftAoB82mDfY0oRe9RU0t4KMUrT9wL8s74ACK4B%2fs1600%2fbroccoli.jpg&ehk=g7eLMymMcibpJDqh%2fxj1tJWeld3lGdv4LAg76SPTrkM%3d&risl=&pid=ImgRaw&r=0', 'https://www.health.com/food/health-benefits-broccoli', 'https://www.delish.com/cooking/nutrition/g241/broccoli-recipes/'),
-(6, 'Bell pepper', 'Bell pepper is a sweet and colorful vegetable.', 'https://th.bing.com/th/id/OIP.wZfzxUAZIGewi3SEIzI43wHaJ4?pid=ImgDet&rs=1', 'https://www.healthline.com/nutrition/foods/bell-peppers', 'https://www.delish.com/cooking/recipe-ideas/g40327395/bell-pepper-recipes/'),
-(7, ' Strawberries', 'Strawberries are sweet, juicy, and vibrant red fruits loved by many.', 'https://th.bing.com/th/id/OIP.fR3uGTXu0iwcQ9jHnItVzQHaLH?pid=ImgDet&rs=1', 'https://www.healthline.com/nutrition/foods/strawberries', 'https://www.delish.com/cooking/g906/strawberry-desserts-round-up/'),
-(8, 'Blueberries', 'Blueberries are small, delicious, and nutritious fruits.', 'https://th.bing.com/th/id/OIP.9wCxblC6uhozuXdGYOFV2AHaFj?pid=ImgDet&rs=1', 'https://www.healthline.com/nutrition/10-proven-benefits-of-blueberries', 'https://www.delish.com/cooking/recipe-ideas/g97/blueberry-desserts-recipes/'); 
+INSERT INTO `plant_details` (`plant_details_id`, `plant_name`, `description`, `image`, `health_benefits`, `recipe`) VALUES
+(1, 'Tomato', 'Tomato is a versatile and delicious fruit commonly used in various culinary dishes.', 'https://i.pinimg.com/originals/f4/db/1b/f4db1bc6273b8479c6c354f325228748.jpg\r\n', 'placeholderlink1', 'placeholderlink1'),
+(2, 'Cauliflower', 'Cauliflower is a nutritious vegetable with a mild, versatile flavor.', 'https://i.pinimg.com/originals/2e/a0/7c/2ea07c91b80166bc23cf1c195c71a260.jpg', 'placeholderlink2', 'placeholderlink2'),
+(3, 'Spinach', 'Spinach is a nutritious leafy green vegetable packed with vitamins and minerals.', 'https://cdn.mos.cms.futurecdn.net/atyrpYQoxdoTzmEgu8HMWE-1200-80.jpg', 'placeholderlink3', 'placeholderlink3'),
+(4, 'Carrot', 'The carrot is a nutritious orange root vegetable.', 'https://i.pinimg.com/originals/d9/a6/1b/d9a61ba54f0381c4bc022aba9a5beefc.jpg', 'placeholderlink4', 'placeholderlink4'),
+(5, 'Broccoli', 'Broccoli is a nutritious green vegetable.', 'https://i.pinimg.com/originals/e6/c1/4e/e6c14edf1247f131db5574bcfa4c626a.jpg', 'placeholderlink5', 'placeholderlink5'),
+(6, 'Bell pepper', 'Bell pepper is a sweet and colorful vegetable.', 'https://i.pinimg.com/originals/cb/06/11/cb06117703f54845979451af286dd6ac.jpg', 'placeholderlink6', 'placeholderlink6'),
+(7, ' Strawberries', 'Strawberries are sweet, juicy, and vibrant red fruits loved by many.', 'https://i.pinimg.com/originals/2b/53/6e/2b536e6e9eb4d488682bfdb2dd17d382.jpg', 'placeholderlink7', 'placeholderlink7'),
+(8, 'Blueberries', 'Blueberries are small, delicious, and nutritious fruits.', 'https://th.bing.com/th/id/OIP.9wCxblC6uhozuXdGYOFV2AHaFj?pid=ImgDet&rs=1', 'placeholderlink8', 'placeholderlink8');
 
 --
 -- Indexes for dumped tables
@@ -69,7 +69,7 @@ ALTER TABLE `plant_details`
 -- AUTO_INCREMENT for table `plant_details`
 --
 ALTER TABLE `plant_details`
-  MODIFY `plant_details_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `plant_details_id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
